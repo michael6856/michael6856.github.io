a0c3f9d9722cf3686d360fd8f2f9d53c1b1e7288
michael6856 <acemichael6856@gmail.com>
2023-04-23 21:37:40 +0800

    0423

---
+++ zh-TW/js/main.js
@@ -0,0 +1,13 @@
+let currentSlide = 0;
+let slides = document.querySelectorAll('.slideshow img');
+let slideInterval = setInterval(nextSlide, 4000);
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+new fullpage('#fullpage', {
+    autocomplete:true
+})
